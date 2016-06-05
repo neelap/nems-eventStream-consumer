213235e9e47190069c78100ae5fd9d8ce50e4673
Bhumika Gogia <bhumika.gogia@wipro.com>
2016-06-05 13:56:28 +0530

    xcvcxb

--- src/main/java/com/nems/stream/consumer/NemsStreamingConsumerApplication.java
+++ src/main/java/com/nems/stream/consumer/NemsStreamingConsumerApplication.java
@@ -2,8 +2,6 @@ package com.nems.stream.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
 public class NemsStreamingConsumerApplication {
